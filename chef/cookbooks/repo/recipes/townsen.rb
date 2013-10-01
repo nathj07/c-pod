14bd5e6dbafcbdc52b861de8724a4feca86f2905
Nick Townsend <townsen@seu-repo.local>
2013-10-01 10:05:57 -0700

    Added Chef account setup for townsen

---
+++ chef/cookbooks/repo/recipes/townsen.rb
@@ -0,0 +1,57 @@
+# A Recipe to setup Nick T.
+#
+group 'townsen' do
+    action :create
+    gid 625
+end
+
+user 'townsen' do
+    action :create
+    comment "Nick Townsend"
+    gid 625
+    uid 625
+    password '$1$ZjqgZlNs$mIqvBcMq6kcUCDsLjyH3I0'
+    supports :manage_home => true
+end
+
+homedir =  node[:etc][:passwd][:townsen][:dir]
+
+cookbook_file "#{homedir}/.vimrc" do
+    source  'vimrc'
+    mode    0644
+    owner   'townsen'
+    group   'townsen'
+end
+
+template "#{homedir}/.gitconfig" do
+    action  :create
+    source  'gitconfig.erb'
+    mode    0644
+    owner   'townsen'
+    group   'townsen'
+    variables( 
+	:useremail => 'nick.townsend@mac.com', :usergecos => 'Nick Townsend' 
+    )
+end
+
+cookbook_file "#{homedir}/.inputrc" do
+    source  'inputrc'
+    mode    0644
+    owner   'townsen'
+    group   'townsen'
+end
+
+directory "#{homedir}/.ssh" do
+    owner   'townsen'
+    group   'townsen'
+    mode    0750
+end
+
+cookbook_file "#{homedir}/.ssh/authorized_keys" do
+    source  'townsen.pub'
+    mode    0644
+    owner   'townsen'
+    group   'townsen'
+end
+
+# vim: sts=4 sw=4 ts=8
