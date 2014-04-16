2eb8a26b04f5630aba0423cbcb185563241e3884
Nick Townsend <nick.townsend@mac.com>
2014-04-16 12:36:39 -0700

    Enable SSL verification for Chef Server
    Updated solo.rb to avoid warning with Chef 11.12.2
    (Even though we don't as yet use Chef Server)

--- www/samples/solo.rb
+++ www/samples/solo.rb
@@ -2,3 +2,4 @@ file_cache_path '/var/chef/cache'
 cookbook_path   '/var/chef/cookbooks'
 recipe_url      'http://<!--#echo var="SERVER_NAME" -->/bin/recipes.cgi'
 json_attribs    '/etc/chef/default.json'
+verify_api_cert true
