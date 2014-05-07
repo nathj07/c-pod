c0ad7c9721f3bd99b919ba30961885bd6d51384b
Nick Townsend <nick.townsend@mac.com>
2014-05-06 21:24:35 -0700

    Added pg-client recipe

---
+++ chef/cookbooks/c-pod/recipes/pg-client.rb
@@ -0,0 +1,14 @@
+# A Recipe to setup PostgreSQL client
+#
+yum_package 'postgresql92-devel'
+# The -devel package (used to compile ruby-pg) installs the following as dependencies:
+# yum_package 'postgresql92-libs'
+# yum_package 'postgresql92-devel'
+
+template "/etc/profile.d/postgres.sh" do
+    mode        0777
+    source      'postgres.sh.erb'
+    variables(  :version => '9.2' )
+end
+
+# vim: sts=4 sw=4 ts=8
