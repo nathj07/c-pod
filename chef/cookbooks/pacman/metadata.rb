e0519788da1ba368dcfb2b055753657b6b0091e2
Nick Townsend <nick.townsend@mac.com>
2014-05-12 10:33:16 -0700

    Use own apache2 cookbook fork
    Opscodes COOK-3900 doesn't work well:
    * Add unused cookbooks required by Apache2 as dummies: empty dirs and metadata

---
+++ chef/cookbooks/pacman/metadata.rb
@@ -0,0 +1,7 @@
+maintainer	    "Dummy"
+maintainer_email    "dummy@email"
+license		    "MIT"
+description	    "Dummy cookbook to satisfy dependencies"
+name		    "pacman"
+version		    "999.9"
+# vim: sts=4 sw=4 ts=8 et
