1eb39b08b6df153adc43ecac176f0054d4754a00
Nick Townsend <nick.townsend@mac.com>
2014-11-26 20:17:22 -0800

    Use bash+install instead of directory resources
    Directory resources give EPERM errors when trying to create directories
    on filesystems mounted over HGFS. Only in Chef. Using install fixes it.

--- chef/cookbooks/c-pod/recipes/repo_structure.rb
+++ chef/cookbooks/c-pod/recipes/repo_structure.rb
@@ -4,73 +4,24 @@ datadir = node[:cpod][:datadir]
 basedir = node[:cpod][:base]
 user = node[:cpod][:owner_name]
 
-directory datadir do
-    mode    02770
-    owner   user
-    group   user
-end
-
 # Setup the YUM repository directories
 #
-%w{ yum_repos}.each do |a|
-    directory File.join(datadir,a) do
-        mode    02770
-        owner   user
-        group   user
-    end
-    %w{ lifted stable unstable }.each do |b|
-        directory File.join(datadir,a,b) do
-            mode    02770
-            owner   user
-            group   user
-        end
-        %w{ 5 6 7 }.each do |c|
-            dir = File.join(datadir,a,b,c)
-            directory dir do
-                mode    02770
-                owner   user
-                group   user
-            end
-            link "#{dir}Server" do
-                to        dir
-                link_type :symbolic
-            end
-            %w{ noarch x86_64 }.each do |d|
-                directory "#{dir}/#{d}" do
-                    mode    02770
-                    owner   user
-                    group   user
-                end
-            end
-        end
-    end
+bash 'setup_repo' do
+    code    "install -m 02770 -o #{user} -g #{user} -d #{datadir}/yum_repos/{lifted,stable,unstable}/{5,6,7}/{noarch,x86_64}"
 end
 
 # Setup the GEM repository directories
 #
-directory File.join(datadir,'gem_repo') do
-    action  :create
-    mode    02770
-    owner   user
-    group   user
-end
-
-directory File.join(datadir,'gem_repo', 'gems') do
-    action  :create
-    mode    02770
-    owner   user
-    group   user
+bash 'setup_gem_repo' do
+    code    "install -m 02770 -o #{user} -g #{user} -d gem_repo/gems"
+    cwd     datadir
 end
 
 # Setup the other directories
 #
-%w{ cookbooks downloads osmirror }.each do |dir|
-    directory File.join(node[:cpod][:datadir], dir) do
-        action  :create
-        mode    02770
-        owner   node[:cpod][:owner_name]
-        group   node[:cpod][:owner_name]
-    end
+bash 'setup_other_dirs' do
+    code    "install -m 02770 -o #{user} -g #{user} -d {cookbooks,downloads,osmirror}"
+    cwd     datadir
 end
 
 # Setup the links
