72dc4c9493737da1be6f14fbed7f2c770f6d0b80
Nick Townsend <nick.townsend@mac.com>
2015-05-21 10:17:21 -0700

    First packaging collateral

---
+++ debbuild/dante-1.4.1/debian/dante.default.ex
@@ -0,0 +1,10 @@
+# Defaults for dante initscript
+# sourced by /etc/init.d/dante
+# installed at /etc/default/dante by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS="-Ddefault"
